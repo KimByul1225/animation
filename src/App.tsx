39150faa7dc35e61bbf121e26b7bdabc91994653
ByulKim <byulkim1225@gmail.com>
2023-02-26 20:09:15 +0900

    variants로 애니메이션 효과 분리

--- src/App.tsx
+++ src/App.tsx
@@ -18,18 +18,18 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars ={ 
+  start: {scale: 0},
+  end: {scale: 1, rotateZ: 360, transition: {type: "spring", damping: 5, delay: 1}}
+}
+
 function App() {
   return (
     <Wrapper>
       <Box
-        initial={{scale: 0}}
-        // Element의 초기값을 지정
-
-        transition={{type: "spring", damping: 5, delay: 1}} 
-        //type은 물리법칙, damping은 반동, stiffness는 경직도 등 다양한 옵션을 설정 할 수 있음.
-
-        animate={{scale: 1, rotateZ: 360}}
-        //최종 스타일
+        variants={myVars}
+        initial="start"
+        animate="end"
       />
     </Wrapper>
   );
